bb60dd9d2cdcdd2e926b1856c8480db68a7bbc36
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-08-25 19:37:33 +0000

    optimization test failed. Aborting change

--- microbenchmark_test2.R
+++ microbenchmark_test2.R
@@ -23,18 +23,36 @@ library(gcipdrtest)
 
 testdat <- mtcars[, 1:4]
 
-# Compare if unification of rmvnorm generation in stochastic integration speeds up routine (expected x2) 
+# Compare if unification of rmvnorm generation in stochastic integration 
+# speeds up routine (expected x2) 
 # (commit: 1c26551488b77a6b5f6e6facc89bd07fb59253df)
+
+custom_check <- function(res){
+  
+  tol <- 0.2 # tolerance parameter
+  x <- res[[1]]
+  y <- res[[2]]
+  
+  gc_param_x <- res[[1]]$copula.parameters
+  gc_param_y <- res[[2]]$copula.parameters
+
+  all(gc_param_x[lower.tri(gc_param_x)] - gc_param_y[lower.tri(gc_param_y)])  
+}
+
 
 res <- microbenchmark(
   {
     set.seed(608, "L'Ecuyer")
-    gcipdr::Simulate.data.given.IPD(testdat, H=5, stochastic.integration = TRUE, SI_k = 50000, method = 3)},
+    gcipdr::Simulate.data.given.IPD(testdat, H=5, stochastic.integration = TRUE, 
+                                    SI_k = 50000, method = 3, checkdata = TRUE,
+                                    tabulate.similar.data = TRUE)},
   {
     set.seed(608, "L'Ecuyer")
-    gcipdrtest::Simulate.data.given.IPD(testdat, H=5, stochastic.integration = TRUE, SI_k = 50000, method = 3)},
-  times = 30L,
-  check = "equal"
+    gcipdrtest::Simulate.data.given.IPD(testdat, H=5, stochastic.integration = TRUE, 
+                                        SI_k = 50000, method = 3, checkdata = TRUE,
+                                        tabulate.similar.data = TRUE)},
+  times = 30L
+  #check = custom_check
 )
 
 print(res)
