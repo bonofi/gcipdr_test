8df2e106e3b1727731ef2cecb59c909da31010a2
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-08-30 20:35:51 +0000

    package build

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,17 +5,20 @@
 #include <Rcpp.h>
 
 using namespace Rcpp;
-using namespace arma;
-using namespace std;
+
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
 
 // findzerocorr
-List findzerocorr(const vec& xell, const vec& x, const double test, const std::string rf_type);
-RcppExport SEXP _gcipdr_findzerocorr(SEXP xellSEXP, SEXP xSEXP, SEXP testSEXP, SEXP rf_typeSEXP) {
+List findzerocorr(const arma::vec& xell, const arma::vec& x, const double test, const std::string rf_type);
+RcppExport SEXP _gcipdrtest_findzerocorr(SEXP xellSEXP, SEXP xSEXP, SEXP testSEXP, SEXP rf_typeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const vec& >::type xell(xellSEXP);
-    Rcpp::traits::input_parameter< const vec& >::type x(xSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type xell(xellSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type x(xSEXP);
     Rcpp::traits::input_parameter< const double >::type test(testSEXP);
     Rcpp::traits::input_parameter< const std::string >::type rf_type(rf_typeSEXP);
     rcpp_result_gen = Rcpp::wrap(findzerocorr(xell, x, test, rf_type));
@@ -24,11 +27,11 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_gcipdr_findzerocorr", (DL_FUNC) &_gcipdr_findzerocorr, 4},
+    {"_gcipdrtest_findzerocorr", (DL_FUNC) &_gcipdrtest_findzerocorr, 4},
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_gcipdr(DllInfo *dll) {
+RcppExport void R_init_gcipdrtest(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
